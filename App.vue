e1cf6f164765141b4faa55927206abb7a8ec784c
Krupa.D <krupa.d@dbbest.com>
2019-04-07 14:05:39 +0300

    add vuex

--- App.vue
+++ App.vue
@@ -1,27 +1,12 @@
 <template>
-  <view class="container">
-    <text class="text-color-primary">{{message}}</text>
-    </view>
+  <app></app>
 </template>
 
 <script>
-  export default {
-    data: function() {
-      return {
-        message: "Hello World"
-      };
-    }
-  };
-</script>
+  import App from "./src/index";
 
-<style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
-</style>
+  export default {
+    components: { App }
+  }
+
+</script>
--- src/index.vue
+++ src/index.vue
@@ -1,5 +1,43 @@
 <template>
-    <root>
-        <app-navigation></app-navigation>
-    </root>
+    <view class="container">
+        <text class="text-color-primary">Login</text>
+        <text-input :style="{height: 40, borderColor: 'gray', borderWidth: 1}" v-model="login"/>
+        <text class="text-color-primary">Password</text>
+        <text-input :style="{height: 40, borderColor: 'gray', borderWidth: 1}" v-model="password"/>
+        <text class="text-color-primary">{{message}}</text>
+        <Button title="Button" :on-press="onButtonTap"/>
+    </view>
 </template>
+
+<script>
+    import store from "./store/index";
+    export default {
+        data: {
+            login: "",
+            password: ""
+        },
+        computed: {
+            message() {
+                return store.getters.getCount
+            }
+        },
+        methods: {
+            onButtonTap: function () {
+                store.dispatch('increment');
+            }
+        }
+    };
+</script>
+
+<style>
+    .container {
+        background-color: white;
+        align-items: center;
+        justify-content: center;
+        flex: 1;
+    }
+
+    .text-color-primary {
+        color: blue;
+    }
+</style>
